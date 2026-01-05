0d530c452e558f15fae1f5927410ae1db0a30f25
Jacen <jacen6916@gmail.com>
2026-01-05 23:44:33 +0800

    NaN

--- train/train_sb3_tea_ppo.py
+++ train/train_sb3_tea_ppo.py
@@ -14,7 +14,7 @@ from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.vec_env import VecNormalize
 
 # 导入我们新定义的组件
-from agent.tea_feature_extractor import TEA_Extractor_V3
+from agent.tea_feature_extractor import TEA_Extractor_V4
 from env.tea_gym_env import TEAGymEnv
 # 复用你原来的 Callback 和 Plot 函数
 from train.train_sb3_transformer_ppo import AttentionVisualizationCallback, plot_rewards
@@ -49,7 +49,7 @@ def main():
 
     # --- 3. 定义 TEA-PPO 策略参数 ---
     policy_kwargs = dict(
-        features_extractor_class=TEA_Extractor_V3,
+        features_extractor_class=TEA_Extractor_V4,
         features_extractor_kwargs=dict(
             features_dim=512,
             embed_dim=128
